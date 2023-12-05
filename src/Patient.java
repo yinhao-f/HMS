eab172d012d8aaa3bfa2c1e021bbdec116f6915e
Yinhao Feng <yf240@sussex.ac.uk>
2023-12-05 01:00:11 +0000

    Patient class v0.1

--- src/Patient.java
+++ src/Patient.java
@@ -4,10 +4,36 @@ public class Patient {
     private String firstName;
     private String lastName;
     private long patientId;
-    private DateFormat patientBirthDate;
+
+    // TODO: store the patient's birth date a Date type so the age will be consistent with time
+    // private Date patientBirthDate;
+    private int age;
     private Gender gender;
     private String notes;
 
+    // TODO: age to be removed
+    public Patient(String firstName, String lastName, long patientId, int age, Gender gender, String notes) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patientId = patientId;
+        this.age = age;
+        this.gender = gender;
+        this.notes = notes;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "Name: " + firstName + ' ' +
+                lastName +
+                ", Patient ID: " + patientId +
+                ", Age: " + age +
+                ", Gender: " + gender +
+                ", Notes: " + notes +
+                '}';
+    }
+
+    // Getters and setters
     public String getFirstName() {
         return firstName;
     }
@@ -32,12 +58,18 @@ public class Patient {
         this.patientId = patientId;
     }
 
-    public DateFormat getPatientBirthDate() {
-        return patientBirthDate;
+//    public void setPatientBirthDate(Date patientBirthDate) {
+//        this.patientBirthDate = patientBirthDate;
+//    }
+
+    // TODO: calculate age based on the birth date and current date
+    public int getAge() {
+        return age;
     }
 
-    public void setPatientBirthDate(DateFormat patientBirthDate) {
-        this.patientBirthDate = patientBirthDate;
+    // TODO: to be removed once the birth date is implemented
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public Gender getGender() {
@@ -54,8 +86,5 @@ public class Patient {
 
     public void setNotes(String notes) {
         this.notes = notes;
-    }
-
-    public Patient() {
     }
 }
